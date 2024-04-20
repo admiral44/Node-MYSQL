c05700b4d720e9cd6b1d92df87e106bb50cdb8c2
Admirl44 <36560341+admiral44@users.noreply.github.com>
2024-04-20 23:17:30 +0530

    formatting.

--- controller/userDetailsController.js
+++ controller/userDetailsController.js
@@ -6,13 +6,8 @@ class userDetailsController {
     static registerUser = async (req, res) => {
         const { name, number, password } = req.body;
 
-        if (!name || !number || !password) {
-            return res.status(400).send({ message: "All fields are required", data: null });
-        }
-
-        if (number.length !== 10) {
-            return res.status(400).send({ message: "Invalid Phone Number", data: null });
-        }
+        if (!name || !number || !password) {return res.status(400).send({ message: "All fields are required", data: null });}
+        if (number.length !== 10) {return res.status(400).send({ message: "Invalid Phone Number", data: null });}
 
         const salt = await bcrypt.genSalt(10);
         const hashPassword = await bcrypt.hash(password, salt);
@@ -30,21 +25,12 @@ class userDetailsController {
 
     static loginUser = async (req, res) => {
         const { number, password } = req.body;
-
-        if (!number || !password) {
-            return res.status(400).send({ message: "All fields are required", data: null });
-        }
+        if (!number || !password) {return res.status(400).send({ message: "All fields are required", data: null });}
 
         await executeQuery("CALL LoginUser(?)", [number]).then(async (results) => {
             if (results.status === 200) {
-
-                const passStatus = await bcrypt.compare(password, results.data[0][0].user_pass).then((passRes) => {
-                    return passRes;
-                });
-                if (!passStatus) {
-                    return res.status(400).send({ message: "Incorrect credentials!!", data: null });
-                }
-
+                const passStatus = await bcrypt.compare(password, results.data[0][0].user_pass).then((passRes) => {return passRes;});
+                if (!passStatus) { return res.status(400).send({ message: "Incorrect credentials!!", data: null });}
                 res.status(200).send({ message: "User Logged In Successfully", data: { id: results.data[0][0].user_id, name: results.data[0][0].user_name } });
             } else {
                 res.status(400).send({ message: results.message, data: null });
